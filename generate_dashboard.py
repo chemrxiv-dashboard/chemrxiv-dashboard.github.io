e3f3346f3b97f546b11c395977720f9aa8f57fad
FX Coudert <fxcoudert@gmail.com>
2020-05-18 01:39:26 +0200

    Improve color scheme

--- generate_dashboard.py
+++ generate_dashboard.py
@@ -165,17 +165,9 @@ var chart = new Chart(ctx, {
     x.append(('Others', n-s))
 
     print(f'labels: {json.dumps([i for i,j in x])},')
-    print('datasets: [{data: ' + json.dumps([j for i, j in x]) + ' }],')
-    print('''
-        },
-    options: {
-        plugins: {
-            colorschemes: {
-                scheme: 'brewer.Paired12'
-            }
-        }
-    }
-    });''')
+    print('datasets: [{data: ' + json.dumps([j for i, j in x]) + ',')
+    print("backgroundColor: ['rgb(31 119 180)', 'rgb(255 127 14)', 'rgb(44 160 44)', 'rgb(214 39 40)', 'rgb(148 103 189)', 'rgb(140 86 75)', 'rgb(227 119 194)', 'rgb(127 127 127)', 'rgb(188 189 34)', 'rgb(23 190 229)', 'rgb(174 199 232)', 'rgb(255 187 120)', 'rgb(152 223 138)', 'rgb(255 152 150)', 'rgb(197 176 213)', 'rgb(196 156 148)', 'rgb(247 182 210)', 'rgb(180 180 180)', 'rgb(219 219 141)', 'rgb(158 218 229)', 'rgb(225 225 225)']}],")
+    print('}});')
     print('</script>')
 
     print('<div id="countryTable">')
